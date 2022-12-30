10c9ad14fc6489c8b477513b1d827caab854242f
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-12-30 15:45:45 +0900

    Fix: testcase > index.php | Added more testcase.

--- testcase/index.php
+++ testcase/index.php
@@ -11,5 +11,25 @@
 /* @var $app    \OP\UNIT\App  */
 /* @var $args    array        */
 //	...
-D( true, false, null, "String", "123", '', "  \t,\r,\n  ", '<h1> XSS ', 0, 123, 1.23, [0, 1, '100', true, false, null,
-		['foo'=>'bar','boolean'=>true,'null'=>null,'string'=>'', 'white-space'=>"  \t,\r,\n  ", 'XSS'=>'<h1> XSS ']]);
+D( true, false, null, TRUE, FALSE, NULL,
+	0, 123, 1.23,
+	'String', '123', 'true', 'false', 'null', 'NULL', '', ' ', '  ', " \t,\r,\n  ", '<h1> XSS ',
+	[
+		0, 1, '100', true, false, null,
+		[
+			'foo'    => 'bar',
+			'null'    => null,
+			'boolean' => [true, false],
+			'number'  => [0, 1, 0.0, 1.0, -0, -1, 1.1],
+			'string'  => [
+				'empty'    => '',
+				'space'    => ' ',
+				'space x2' => '  ',
+				'quote'    => '"',
+				'single quote' => "'",
+				'white space'=>"  \t,\r,\n  ",
+				'XSS'=>'<h1> XSS ',
+			]
+		]
+	]
+);
