fc83280a84545e3fd202c019e92a9b3dbd67156a
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-11-23 00:49:42 +0900

    Fix: ci > Dump.php | Added white space.

--- ci/Dump.php
+++ ci/Dump.php
@@ -32,7 +32,7 @@ $args   = new \stdClass();
 $ci->Set('_Object', $result, $args);
 
 //	Mark
-$result = "unit:/ci/CI.class.php #509 - ";
+$result = "unit:/ci/CI.class.php   #509 - ";
 $args   = [];
 $ci->Set('Mark', $result, $args);
 
